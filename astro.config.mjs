3e4bbf8ac4f865184ffa05f9d0ce9ebe6b697fc8
Friedrich Lasnia <f.lasnia@handelsblattgroup.com>
2022-09-24 19:41:06 +0200

    basic menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
@@ -7,5 +7,10 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), mdx(), sitemap()]
+  integrations: [tailwind(), image(), mdx(), sitemap()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
